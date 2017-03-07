412520775adc9d6c39065f70ac6711694ea2d457
Noy <noyhillel@gmail.com>
2017-03-07 08:50:35 +0000

    typo..

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func start()  {
 	http.HandleFunc("/", func (responseWriter http.ResponseWriter, request *http.Request) {
 		ip, err := powerweb.Nest(nil, request)
 		if err != nil { panic(err) }
-		// just wanted to print something basic on the 'home page'
+		// HTML which includes a header and some content so the front page won't be empty. It's always better to make a static folder.
 		fmt.Fprint(responseWriter, "<head><link href=\"https://fonts.googleapis.com/css?family=Source+Code+Pro\" rel=\"stylesheet\"></head>" +
 			"<style> body { background-color: #E27B65; text-align: center; position: relative; font-family: 'Source Code Pro', monospace;" +
 			"font-size: 30px;} </style>")
